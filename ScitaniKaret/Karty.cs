1c205861a0584be33a7b885fb224c57f10838cba
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-11-29 12:50:58 +0100

    Add project files.

---
+++ ScitaniKaret/Karty.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScitaniKaret
+{
+    class Karty
+    {
+        
+        public void PocetKaret()
+        {
+            Console.WriteLine("Zadejte počet dávek: ");
+            int pocetDavek = int.Parse(Console.ReadLine());
+            int[] pocetKaret = new int[pocetDavek];
+            Console.WriteLine("Zadejte počet karet: ");
+            for (int i = 0; i < pocetDavek; i++)
+                {
+                    pocetKaret[i] = int.Parse(Console.ReadLine());
+                }
+            int suma = pocetKaret.Sum();
+            Console.WriteLine("Celkový počet karet je: " + suma);
+            
+
+
+        }
+
+    }
+}
---
+++ ScitaniKaret/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ScitaniKaret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karty karty = new Karty();
+            karty.PocetKaret();
+            Console.ReadKey();
+        }
+    }
+}
